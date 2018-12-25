0405e1b35eb572600a66aa4817c61cc2a487e221
Nayra-Adel <nayra.adel128@stud.fci-cu.edu.eg>
2018-12-25 20:05:36 +0200

    Autosubmit "automatically submit candidate's answers after time runs out"

--- Model/hr.js
+++ Model/hr.js
@@ -17,7 +17,7 @@ module.exports = {
       connection.query('SELECT user.user_id, username, email, cv, type, question_sentence, user_answer, correctAnswer FROM online_exams_db.user JOIN online_exams_db.pass_exam ON pass_exam.user_id = user.user_id JOIN online_exams_db.exam ON exam.exam_id=pass_exam.exam_id JOIN online_exams_db.exam_question ON exam.exam_id=exam_question.exam_id JOIN online_exams_db.question ON question.question_id=exam_question.question_id JOIN online_exams_db.solve_question ON exam_question.question_id=solve_question.question_id where user.user_id=solve_question.user_id and exam.type = ? order by user.user_id',
         [type], function (error, result) {
         if (error == 'null'){
-            eventEmitter.emit('not-search-type');     
+            eventEmitter.emit('not-search-type');       
         }
         else{
             eventEmitter.emit('search-type', result);
